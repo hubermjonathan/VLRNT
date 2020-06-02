724bd2ea9efba8ac36a748ead9840dd50893d343
Jon Huber <hubermjonathan@gmail.com>
2020-06-01 17:21:48 -0700

    updated flask app for heroku

--- image_hoster.py
+++ image_hoster.py
@@ -3,3 +3,6 @@ app = Flask(__name__)
 @app.route('/<image>')
 def send_image(image):
 	return send_file(f'assets/{image}.png', mimetype='image/gif')
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
