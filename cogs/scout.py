680ba712ca11c7ab5dfbb721ea0a5c7ffc61ea46
Jon Huber <hubermjonathan@gmail.com>
2020-06-02 17:46:01 -0700

    updated scout skeleton

--- cogs/scout.py
+++ cogs/scout.py
@@ -11,28 +11,28 @@ class Scout(commands.Cog):
         self.bot = bot
 
     @commands.command(aliases=['s'])
+    @commands.bot_has_permissions(send_messages=True, embed_links=True)
     async def scout(self, ctx, riot_id):
-        # TODO verify riot id
-        if False:
-            raise commands.BadArgument('Provide a valid Riot ID')
+        # TODO verify riot id, verify in game
 
         # create the embed
         embed = discord.Embed(title=riot_id, description='Scout Data', color=globals.embed_color)
+        embed.set_thumbnail(url='https://hubermjonathan-vlrnt.herokuapp.com/haven')
         embed.add_field(name='**Your Team**',
-            value=f'**Sage (Iron 3):** 53% W/L 0.87 K/D\n' \
-                f'**Sova (Iron 2):**  56% W/L 1.05 K/D\n' \
-                f'**Breach (Bronze 3):**  48% W/L 0.63 K/D\n' \
-                f'**Phoenix (Iron 1):**  51% W/L 0.95 K/D\n' \
-                f'**Brimstone (Silver 2):**  52% W/L 1.12 K/D\n',
-            inline=True
+            value=f'**Player 1 - Sage (Iron 3):** 53% W/L - 0.87 K/D\n' \
+                f'**Player 2 - Sova (Iron 2):**  56% W/L - 1.05 K/D\n' \
+                f'**Player 3 - Breach (Bronze 3):**  48% W/L - 0.63 K/D\n' \
+                f'**Player 4 - Phoenix (Iron 1):**  51% W/L - 0.95 K/D\n' \
+                f'**Player 5 - Brimstone (Silver 2):**  52% W/L - 1.12 K/D\n',
+            inline=False
         )
         embed.add_field(name='**Enemy Team**',
-            value=f'**Sova (Iron 3):** 53% W/L 0.87 K/D\n' \
-                f'**Breach (Iron 2):**  56% W/L 1.05 K/D\n' \
-                f'**Cypher (Bronze 3):**  48% W/L 0.63 K/D\n' \
-                f'**Reyna (Iron 1):**  51% W/L 0.95 K/D\n' \
-                f'**Sage (Silver 2):**  52% W/L 1.12 K/D\n',
-            inline=True
+            value=f'**Player 1 - Sova (Iron 3):** 53% W/L - 0.87 K/D\n' \
+                f'**Player 2 - Breach (Iron 2):**  56% W/L - 1.05 K/D\n' \
+                f'**Player 3 - Cypher (Bronze 3):**  48% W/L - 0.63 K/D\n' \
+                f'**Player 4 - Reyna (Iron 1):**  51% W/L - 0.95 K/D\n' \
+                f'**Player 5 - Sage (Silver 2):**  52% W/L - 1.12 K/D\n',
+            inline=False
         )
         embed.set_footer(text='Because the VALORANT API is still in development, no real data is used for this command.')
 
