0faf7a6affdd880311bd17a4089ebaad01b204d6
Jon Huber <hubermjonathan@gmail.com>
2020-06-02 19:12:00 -0700

    updated history skeleton

--- cogs/history.py
+++ cogs/history.py
@@ -1,3 +1,4 @@
+import random
 import discord
 from discord.ext import commands
 import globals
@@ -9,36 +10,6 @@ def setup(bot):
 class History(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    async def get_match_history(self, ctx, riot_id, games):
-        # create the embed
-        embed = discord.Embed(title=riot_id, description='Match History', color=globals.embed_color)
-        for i, game in enumerate(games):
-            embed.add_field(name=f'**Game {i+1}**',
-                value=f'**{game["map"]} - {game["result"]}**\n'
-                    f'{game["score"]}\n'
-                    f'Combat Score: {game["avg_combat_score"]}\n'
-                    f'Score: {game["kills"]}/{game["deaths"]}\n',
-                inline=False
-            )
-        embed.set_footer(text='Because the VALORANT API is still in development, no real data is used for this command.')
-
-        # send the message
-        await ctx.send(embed=embed)
-
-    async def get_single_game(self, ctx, riot_id, game):
-        # create the embed
-        embed = discord.Embed(title=riot_id, color=globals.embed_color)
-        embed.add_field(name=f'**{game["map"]} - {game["result"]}**\n',
-            value=f'{game["score"]}\n'
-                f'Combat Score: {game["avg_combat_score"]}\n'
-                f'Score: {game["kills"]}/{game["deaths"]}\n',
-            inline=False
-        )
-        embed.set_footer(text='Because the VALORANT API is still in development, no real data is used for this command.')
-
-        # send the message
-        await ctx.send(embed=embed)
 
     @commands.command(aliases=['h', 'hist'])
     @commands.bot_has_permissions(send_messages=True, embed_links=True)
@@ -57,38 +28,38 @@ class History(commands.Cog):
 
         # TODO verify riot id
 
-        # TODO gather the data
-        games = [
-            {
-                'map': 'Bind',
-                'score': '13-12',
-                'result': 'Victory',
-                'kills': 24,
-                'deaths': 11,
-                'avg_combat_score': 365
-            },
-            {
-                'map': 'Split',
-                'score': '11-13',
-                'result': 'Defeat',
-                'kills': 17,
-                'deaths': 19,
-                'avg_combat_score': 243
-            },
-            {
-                'map': 'Haven',
-                'score': '13-6',
-                'result': 'Victory',
-                'kills': 28,
-                'deaths': 9,
-                'avg_combat_score': 413
-            }
-        ]
-
+        # send full match history
         if len(args) == 1:
-            # send full match history
-            await self.get_match_history(ctx, args[0], None)
+            # create the embed
+            embed = discord.Embed(title=args[0], description='Match History', color=globals.embed_color)
+            for i in range(3):
+                map = random.choice(["Bind", "Haven", "Split", "Ascent"])
+                embed.add_field(name=f'**Game {i+1}**',
+                    value=f'**{map} - {random.choice(["Victory", "Defeat"])}**\n'
+                        f'{random.choice(["13-12", "7-13", "13-10", "13-4", "9-13"])}\n'
+                        f'Combat Score: {random.randrange(200, 400)}\n'
+                        f'Score: {random.randrange(7, 30)}/{random.randrange(7, 20)}/{random.randrange(0, 10)}\n',
+                    inline=False
+                )
+            embed.set_footer(text='Because the VALORANT API is still in development, no real data is used for this command.')
+
+            # send the message
+            await ctx.send(embed=embed)
+        
+        # send single game information
         else:
-            # send single game information
-            await self.get_single_game(ctx, args[0], None)
+            # create the embed
+            embed = discord.Embed(title=args[0], color=globals.embed_color)
+            map = random.choice(["Bind", "Haven", "Split", "Ascent"])
+            embed.set_thumbnail(url=f'https://hubermjonathan-vlrnt.herokuapp.com/{map}')
+            embed.add_field(name=f'**{map} - {random.choice(["Victory", "Defeat"])}**\n',
+                value=f'{random.choice(["13-12", "7-13", "13-10", "13-4", "9-13"])}\n'
+                    f'Combat Score: {random.randrange(200, 400)}\n'
+                    f'Score: {random.randrange(7, 30)}/{random.randrange(7, 20)}/{random.randrange(0, 10)}\n',
+                inline=False
+            )
+            embed.set_footer(text='Because the VALORANT API is still in development, no real data is used for this command.')
+
+            # send the message
+            await ctx.send(embed=embed)
 
