f5fb4b67b6aa0c1876c88d9299d2a859f740a89e
Jon Huber <hubermjonathan@gmail.com>
2020-06-02 18:43:00 -0700

    get req converted to lowercase for asset hoster

--- asset_hoster.py
+++ asset_hoster.py
@@ -7,7 +7,7 @@ def send_verification():
 @app.route('/<image>')
 def send_image(image):
     if image == 'favicon.ico': return ('', 404)
-    return send_file(f'assets/{image}.png', mimetype='image/gif')
+    return send_file(f'assets/{image.lower()}.png', mimetype='image/gif')
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
