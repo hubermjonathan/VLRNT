2c55cc895f3af14ce10bfa1d7875cbde85860e1d
Jon Huber <hubermjonathan@gmail.com>
2020-06-16 12:55:16 -0700

    added event for guild join

--- cogs/admin.py
+++ cogs/admin.py
@@ -13,12 +13,30 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        # get the number of guilds
+        num_guilds = len(self.bot.guilds) - 2
+
+        # get the number of users
+        members = []
+        for g in self.bot.guilds:
+            if g.id == 110373943822540800 or g.id == 264445053596991498:
+                continue
+            for m in g.members:
+                if not m.bot and m.id not in members:
+                    members.append(m.id)
+        num_members = len(members)
+
+        # send the message
+        await self.bot.get_user(196141424318611457).send(embed=discord.Embed(title='New Guild', description=f'**New guild name:** {guild.name}\n**Number of guilds:** {num_guilds}\n**Number of members:** {num_members}\n', color=globals.embed_color))
+
     @commands.command()
     @commands.is_owner()
     @commands.dm_only()
     async def stats(self, ctx):
         # get the number of guilds
-        num_guilds = len(self.bot.guilds)
+        num_guilds = len(self.bot.guilds) - 2
 
         # get the number of users
         members = []
